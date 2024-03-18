57163a05475400740970da4610f3868b5ac5f519
sharhar <wiishahar@gmail.com>
2024-03-18 03:20:54 +0000

    linux compatibility

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export VULKAN_SDK="/Users/shaharsandhaus/VulkanSDK/1.3.275.0/macOS"
+export VULKAN_SDK="/home/shaharsandhaus/1.3.204.1/x86_64"
 export PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 python3 -m build
